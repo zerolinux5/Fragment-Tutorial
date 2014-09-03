5aba88c3c0c7462148ac8a4f9d7da4416765580a
Jesus Magana <maganazero5@gmail.com>
2014-09-02 20:22:23 -0700

    Starting code

---
+++ gen/com/ZeroLinux5/rssreader/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ZeroLinux5.rssreader;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/ZeroLinux5/rssreader/RssfeedActivity.java
@@ -0,0 +1,15 @@
+package com.ZeroLinux5.rssreader;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class RssfeedActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_rssfeed);
+	}
+}

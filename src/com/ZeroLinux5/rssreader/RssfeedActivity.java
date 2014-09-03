92d312678294608b5409b240b629d2bb92a528e2
Jesus Magana <maganazero5@gmail.com>
2014-09-02 20:38:08 -0700

    Added to main class and fixed class used

--- src/com/ZeroLinux5/rssreader/RssfeedActivity.java
+++ src/com/ZeroLinux5/rssreader/RssfeedActivity.java
@@ -1,15 +1,26 @@
 package com.ZeroLinux5.rssreader;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.Activity;
 
-public class RssfeedActivity extends Activity {
+public class RssfeedActivity extends Activity implements MyListFragment.OnItemSelectedListener{
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_rssfeed);
-	}
-}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rssfeed);
+    }
+
+    // if the wizard generated an onCreateOptionsMenu you can delete
+    // it, not needed for this tutorial
+
+  @Override
+  public void onRssItemSelected(String link) {
+    DetailFragment fragment = (DetailFragment) getFragmentManager()
+            .findFragmentById(R.id.detailFragment);
+        if (fragment != null && fragment.isInLayout()) {
+          fragment.setText(link);
+        } 
+  }
+    
+} 
